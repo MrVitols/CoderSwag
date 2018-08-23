5f871002d900c3ea35c52a3831eb809e77dd0b14
Vitols, Martins <martins.vitols@accenture.com>
2018-08-23 17:03:59 +0300

    lesson 1

--- app/src/main/java/com/martinsvitols/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/martinsvitols/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.martinsvitols.coderswag
+package com.martinsvitols.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.martinsvitols.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/martinsvitols/coderswag/Model/Product.kt
@@ -0,0 +1,3 @@
+package com.martinsvitols.coderswag.Model
+
+class Product(val title: String, val price: String, val image: String)
